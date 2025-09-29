e9c6132ff31baeb8d10483a90e8da3af0f43f3f9
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2025-09-29 15:22:37 -0300

    Update auth.ts

--- src/views/auth/auth.ts
+++ src/views/auth/auth.ts
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import { showForgotPassword, handleForgotPassword, showResetPassword, handleResetPassword } from "../controllers/AuthController";
+
+const router = Router();
+
+router.get("/forgot-password", showForgotPassword);
+router.post("/forgot-password", handleForgotPassword);
+router.get("/reset-password/:token", showResetPassword);
+router.post("/reset-password/:token", handleResetPassword);
+
+export default router;
 
